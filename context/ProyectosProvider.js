1df0131eb29fecad913b53ab601371ee2293dedb
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-12-02 22:11:02 -0500

    mejoras de entradas de proyectos

--- context/ProyectosProvider.js
+++ context/ProyectosProvider.js
@@ -1,38 +1,36 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react'
 
-const ProyectosContext = createContext();
+const ProyectosContext = createContext()
 
 const ProyectosProvider = ({ children }) => {
-  const [proyectos, setProyectos] = useState([]);
-  const [entradaProyectos, setEntradaProyectos] = useState([]);
+  const [proyectos, setProyectos] = useState([])
+  const [entradaProyectos, setEntradaProyectos] = useState([])
 
   const consultarProyectos = async () => {
     try {
-      const respuesta = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/proyectos`
-      );
-      const data = await respuesta.json();
-      const entrada = data.slice(0, 6);
-      setEntradaProyectos(entrada);
+      const respuesta = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/proyectos`)
+      const data = await respuesta.json()
+      const entrada = data.reverse().slice(0, 6)
+      setEntradaProyectos(entrada)
       // Ordernar al reves
-      const proyectos = data.reverse();
-      setProyectos(proyectos);
+      const proyectos = data.reverse()
+      setProyectos(proyectos)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    consultarProyectos();
-  }, []);
+    consultarProyectos()
+  }, [])
 
   return (
     <ProyectosContext.Provider value={{ proyectos, entradaProyectos }}>
       {children}
     </ProyectosContext.Provider>
-  );
-};
+  )
+}
 
-export { ProyectosProvider };
+export { ProyectosProvider }
 
-export default ProyectosContext;
+export default ProyectosContext
